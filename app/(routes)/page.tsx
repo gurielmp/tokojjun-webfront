d31e2a32ec4e25c93d7132ca2e5c8ba0d8a626e2
gurielamp <gurielamp@gmail.com>
2024-02-01 19:54:29 +0700

    connect to categories and billboard API

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,8 +1,18 @@
-const HomePage = () => {
+import getBillboard from "@/actions/get-billboard"
+import Billboard from "@/components/billboard"
+import Container from "@/components/ui/container"
+
+export const revalidate = 0
+
+const HomePage = async () => {
+  const billboard = await getBillboard("b92e7c56-56f6-4611-b1d3-7e874fb94f60")
+
   return (
-    <div>
-      <h1>Home Tokojjun</h1>
-    </div>
+    <Container>
+      <div className="space-y-10 pv-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
   )
 }
 
