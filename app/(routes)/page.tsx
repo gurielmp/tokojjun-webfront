053072a620b637e91b8be909448311054a6fc535
gurielamp <gurielamp@gmail.com>
2024-02-01 17:13:35 +0700

    setup web front

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Home Tokojjun</h1>
+    </div>
+  )
+}
+
+export default HomePage
---
+++ components/navbar.tsx
@@ -0,0 +1,5 @@
+const Navbar = () => {
+  return <div className="border-b">navbar</div>
+}
+
+export default Navbar
