7e50f2a6503c65c13e6173f692a523926a6eeea3
gurielamp <gurielamp@gmail.com>
2024-02-06 18:37:41 +0700

    feat: create toast for cart

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -4,6 +4,7 @@ import { ShoppingBag } from "lucide-react"
 import { useEffect, useState } from "react"
 
 import Button from "@/components/ui/button"
+import useCart from "@/hooks/use-cart"
 
 const NavbarActions = () => {
   const [isMounted, setIsMounted] = useState(false)
@@ -11,6 +12,8 @@ const NavbarActions = () => {
   useEffect(() => {
     setIsMounted(true)
   }, [])
+
+  const cart = useCart()
 
   if (!isMounted) {
     return null
@@ -20,7 +23,9 @@ const NavbarActions = () => {
     <div className="ml-auto flex items-center gap-x-4">
       <Button className="flex items-center rounded-full bg-black px-4 py-2">
         <ShoppingBag size={20} color="white" />
-        <span className="ml-2 text-sm font-medium text-white">0</span>
+        <span className="ml-2 text-sm font-medium text-white">
+          {cart?.items.length}
+        </span>
       </Button>
     </div>
   )
