218058f4176937ae8512980703b24206e5ab5b5d
gurielamp <gurielamp@gmail.com>
2024-02-11 01:22:29 +0700

    feat: create summary and checkout

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -1,9 +1,12 @@
 "use client"
 
+import { useEffect, useState } from "react"
+
 import Container from "@/components/ui/container"
 import useCart from "@/hooks/use-cart"
-import { useEffect, useState } from "react"
+
 import CartItem from "./components/cart-item"
+import Summary from "./components/summary"
 
 const CartPage = () => {
   const cart = useCart()
@@ -16,11 +19,11 @@ const CartPage = () => {
   if (!isMounted) return null
 
   return (
-    <div className="bg-white ">
+    <div className="bg-white">
       <Container>
-        <div className="px-4 sm:px-6 lg:px-8">
+        <div className="px-4 py-16 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold text-black">Shopping Cart</h1>
-          <div className="mt-12 lg:grid lg:grid-col-12 lg:items-start gap-x-12">
+          <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
             <div className="lg:col-span-7">
               {cart.items.length === 0 && (
                 <p className="text-neutral-500">No items added to cart</p>
@@ -31,6 +34,7 @@ const CartPage = () => {
                 ))}
               </ul>
             </div>
+            <Summary />
           </div>
         </div>
       </Container>
